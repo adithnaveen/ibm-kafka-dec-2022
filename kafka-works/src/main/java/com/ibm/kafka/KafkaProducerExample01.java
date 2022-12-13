3bb2a28bfd83bda4ee0e877c2c0fe75edab6a494
Naveen <adith.naveen@gmail.com>
2022-12-13 15:15:26 +0530

    producer java kafka

--- kafka-works/src/main/java/com/ibm/kafka/KafkaProducerExample01.java
+++ kafka-works/src/main/java/com/ibm/kafka/KafkaProducerExample01.java
@@ -2,9 +2,11 @@ package com.ibm.kafka;
 
 import java.util.Properties;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,14 +30,30 @@ public class KafkaProducerExample01 {
 		ProducerRecord<String, String> producerRecord = 
 				new ProducerRecord<String, String>("ibm-java-topic", "Hello World From Java1");
 		
-		
-		producer.send(producerRecord);
+		producer.send(producerRecord, new Callback() {
+			
+			@Override
+			public void onCompletion(RecordMetadata metadata, Exception exception) {
+				if(exception == null) {
+					log.info("----------------------------------------------");
+					log.info("Message Sent Successfully");
+					log.info("Topic " + metadata.topic() + 
+								"\nPartition " + metadata.partition() + 
+								"\nTime Stamp " + metadata.timestamp() +
+								"\nOffSet " + metadata.offset()); 
+					
+				}else {
+					log.error("Sorry! Exception while posting message {}", exception);
+				}
+			}
+		});
 		
 		producer.flush(); 
 		producer.close();
 		
-		log.info("Message Sent");
+		
 		
 		
 	}
 }
+
